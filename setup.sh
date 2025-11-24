43dc235c35efcb067fe5ffd3fd39d3cb2824e250
akshityadav-maker <akshityadav989@gmail.com>
2025-11-24 18:06:24 +0530

    fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Starting ERP Database Setup..."
 read -sp "Enter MySQL root password: " ROOTPASS
-echo ""
+echo "ROOTPASS"
 
-sudo mariadb -u root -p"ROOTPASS" < ./src/resources/auth_db.sql
-sudo mariadb -u root -p"ROOTPASS" < ./src/resources/erp_db.sql
-sudo mariadb -u root -p"ROOTPASS" < ./src/resources/sample_data.sql
+sudo mariadb -u root -p"$ROOTPASS" < ./src/resources/auth_db.sql
+sudo mariadb -u root -p"$ROOTPASS" < ./src/resources/erp_db.sql
+sudo mariadb -u root -p"$ROOTPASS" < ./src/resources/sample_data.sql
